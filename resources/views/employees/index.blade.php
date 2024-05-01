a0a5c9e8947e87e495a7efe83913610120cba55c
Era Ismaili <eraaismaili@gmail.com>
2024-05-01 17:15:55 +0200

    dropdown list

--- resources/views/employees/index.blade.php
+++ resources/views/employees/index.blade.php
@@ -95,6 +95,22 @@
             </ul>
         </nav>
     </header>
+    <script>
+        document.addEventListener("DOMContentLoaded", function() {
+            var dropdownToggle = document.querySelector('.dropdown-toggle');
+            var dropdownMenu = document.querySelector('.dropdown-menu');
+
+            dropdownToggle.addEventListener('click', function() {
+                dropdownMenu.classList.toggle('show');
+            });
+
+            window.addEventListener('click', function(event) {
+                if (!dropdownToggle.contains(event.target) && !dropdownMenu.contains(event.target)) {
+                    dropdownMenu.classList.remove('show');
+                }
+            });
+        });
+    </script>
 
     <body>
         <div class="container">
